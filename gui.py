941debaf51fc0414f01c7123b9df6e34adf70dbc
Martin Kroken Vig <martinkv05@gmail.com>
2023-02-01 09:53:59 +0100

    cloning + x and y references for each clone

--- gui.py
+++ gui.py
@@ -1,19 +1,27 @@
 from tkinter import *
-from jsonfile import *
+from jsonfile import readvalue
 from PIL import ImageTk, Image
-clonenr = 0
+cloneam = 0
+# cloneamout
 clones = []
 
 
 def mkclone(img, x, y):
-    global clonenr
-    print(clonenr)
-    clonenr += 1
+    global cloneam
+    cloneam += 1
     cloneimage = ImageTk.PhotoImage(Image.open(img).resize((132, 100), Image.LANCZOS))
     cloneitem = Label(frame, image=cloneimage, bd=0, cursor="hand2")
     cloneitem.place(x=x, y=y)
     cloneitem.image = cloneimage
+    cloneitem.bind("<Button-1>", lambda e: moveclone(cloneitem))
     clones.append(cloneitem)
+    clones.append([x, y])
+
+
+def moveclone(name):
+    clonex = str(name)
+    clonex = int(clonex.split("l")[2])-6
+    print(clonex)
 
 
 # values read from .json file
@@ -22,7 +30,7 @@ azimuth = readvalue("azimuth")
 acttunnel, actazimuth = 0, 0
 window = Tk()
 window.title("Bridge designer")
-tunnelimg, azimuthimg, tunnelx2img, tunnel2xdpimg = "assets/BC-5-panel-tunnel.png", 'assets/BC-5-panel-azimuth.png', 'assets/BC-5-panel-two-tunnels.png', 'assets/BC-5-panel-two-tunnels-dp.png'
+images = ["assets/BC-5-panel-tunnel.png", 'assets/BC-5-panel-azimuth.png', 'assets/BC-5-panel-two-tunnels.png', 'assets/BC-5-panel-two-tunnels-dp.png']
 
 # makes window (almoast) fullscreen
 window.attributes("-topmost", True)
@@ -34,22 +42,22 @@ frame = Frame(window, bg='grey', width=200, height=window.winfo_screenheight())
 frame.grid(row=0, column=0)
 frame.grid_propagate(False)
 
-menuimage_1 = ImageTk.PhotoImage(Image.open(tunnelimg).resize((132, 100), Image.LANCZOS))
+menuimage_1 = ImageTk.PhotoImage(Image.open(images[0]).resize((132, 100), Image.LANCZOS))
 menuitem_1 = Label(frame, image=menuimage_1, bd=0, cursor="hand2")
 menuitem_1.place(relx=0.5, rely=0.15, anchor=CENTER)
 
-menuimage_2 = ImageTk.PhotoImage(Image.open(azimuthimg).resize((132, 100), Image.LANCZOS))
+menuimage_2 = ImageTk.PhotoImage(Image.open(images[1]).resize((132, 100), Image.LANCZOS))
 menuitem_2 = Label(frame, image=menuimage_2, bd=0, cursor="hand2")
 menuitem_2.place(relx=0.5, rely=0.3, anchor=CENTER)
 
-menuimage_3 = ImageTk.PhotoImage(Image.open(tunnelx2img).resize((132, 100), Image.LANCZOS))
+menuimage_3 = ImageTk.PhotoImage(Image.open(images[2]).resize((132, 100), Image.LANCZOS))
 menuitem_3 = Label(frame, image=menuimage_3, bd=0, cursor="hand2")
 menuitem_3.place(relx=0.5, rely=0.45, anchor=CENTER)
 
-menuimage_4 = ImageTk.PhotoImage(Image.open(tunnel2xdpimg).resize((132, 100), Image.LANCZOS))
+menuimage_4 = ImageTk.PhotoImage(Image.open(images[3]).resize((132, 100), Image.LANCZOS))
 menuitem_4 = Label(frame, image=menuimage_4, bd=0, cursor="hand2")
 menuitem_4.place(relx=0.5, rely=0.6, anchor=CENTER)
-menuitem_4.bind("<Button-1>", lambda e: mkclone(tunnel2xdpimg, 100, 100))
+menuitem_4.bind("<Button-1>", lambda e: mkclone(images[3], 100, 100))
 menutitle = Label(frame, text="Control panels")
 menutitle.place(relx=0.5, rely=0.05, anchor=CENTER)
 
