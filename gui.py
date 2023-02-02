333efe9e10903dea9209c2f20759e86adafc3a1b
Martin Kroken Vig <martinkv05@gmail.com>
2023-02-02 15:53:00 +0100

    cls, oppdatert json og validering

--- gui.py
+++ gui.py
@@ -2,27 +2,35 @@ import tkinter.messagebox
 from tkinter import *
 from jsonfile import readvalue
 from PIL import ImageTk, Image
+
 global clones
 clones = []
+screens = False
 
 
 def color(tt, acttt):
-    if tt != acttt:
+    if tt == 0 and acttt == 0:
+        return "black"
+    elif tt != acttt:
         return "red"
     else:
         return "green"
 
 
 def cls():
-    res = tkinter.messagebox.askokcancel('Clear screen?', 'All unsaved progress will be lost')
-    if res == 'yes':
+
+    if tkinter.messagebox.askokcancel('Clear screen?', 'All unsaved progress will be lost'):
         for clone in clones:
             delclone(clone[0], clone[1])
         clones.clear()
+        for i in thrusters:
+            i[0] = 0
+        validate()
 
 
 def validate():
     size = 19
+
     menuitem_5 = Label(framel, text=f"Azimuth: {thrusters[1][0]}/{thrusters[1][1]}", font=("", size), fg=color(thrusters[1][0], thrusters[1][1]), bg="grey")
     menuitem_5.place(relx=0.5, rely=0.15, anchor=CENTER)
 
@@ -47,37 +55,14 @@ def mkclone(img, x, y, xs, ys, offsetx, offsety, tt):
     cloneitem.bind("<B1-Motion>", lambda e: moveclone(cloneitem, offsetx, offsety))
     cloneitem.bind("<Button-3>", lambda e: delclone(cloneitem, tt))
     cloneitem.image = cloneimage
-    if tt == "azimuth":
-        thrusters[1][0] += 1
-    elif tt == "tunnel":
-        thrusters[0][0] += 1
-    elif tt == "tunnel-split-handle":
-        thrusters[2][0] += 1
-    elif tt == "tunnel-dual-screen":
-        thrusters[3][0] += 1
-    elif tt == "azimuth-propulsion":
-        thrusters[4][0] += 1
-    else:
-        pass
+    thrusters[tt][0] += 1
     clones.append([cloneitem, tt])
     validate()
 
 
 def delclone(name, tt):
-    global thrusters
     name.place_forget()
-    if tt == "azimuth":
-        thrusters[1][0] -= 1
-    elif tt == "tunnel":
-        thrusters[0][0] -= 1
-    elif tt == "tunnel-split-handle":
-        thrusters[2][0] -= 1
-    elif tt == "tunnel-dual-screen":
-        thrusters[3][0] -= 1
-    elif tt == "azimuth propulsion":
-        thrusters[4][0] -= 1
-    else:
-        pass
+    thrusters[tt][0] -= 1
     validate()
 
 
@@ -92,6 +77,7 @@ def moveclone(name, offsetx, offsety):
 thrusters = [[0, readvalue("tunnel")], [0, readvalue("azimuth")], [0, readvalue("tunnel-split-handle")], [0, readvalue("tunnel-dual-screen")], [0, readvalue("azimuth-propulsion")]]
 window = Tk()
 window.title("Bridge designer")
+
 images = ["assets/BC-5-panel-tunnel.png", 'assets/BC-5-panel-azimuth.png', 'assets/BC-5-panel-two-tunnels.png', 'assets/BC-5-panel-two-tunnels-dp.png']
 
 # makes window (almoast) fullscreen
@@ -106,35 +92,78 @@ framel = Frame(window, bg='grey', width=300, height=window.winfo_screenheight())
 framel.grid(row=0, column=3)
 framel.grid_propagate(False)
 
+
 clsimg = ImageTk.PhotoImage(Image.open("assets/cls-light.png").resize((20, 25), Image.LANCZOS))
 clsicon = Label(window, image=clsimg, bd=0, cursor="hand2")
 clsicon.place(x=210, y=25)
 clsicon.bind("<Button-1>", lambda e: cls())
 
-validate()
-menuimage_1 = ImageTk.PhotoImage(Image.open(images[0]).resize((132, 100), Image.LANCZOS))
-menuitem_1 = Label(frame, image=menuimage_1, bd=0, cursor="hand2")
-menuitem_1.place(relx=0.5, rely=0.3, anchor=CENTER)
-menuitem_1.bind("<Button-1>", lambda e: mkclone(images[0], 40, 170, 132, 100, 70, 50, "tunnel"))
-menutitle = Label(frame, text="Control panels", font=("", 16))
-menutitle.place(relx=0.5, rely=0.05, anchor=CENTER)
 menutitle = Label(framel, text="Thruster types", font=("", 16))
 menutitle.place(relx=0.5, rely=0.05, anchor=CENTER)
 
-menuimage_2 = ImageTk.PhotoImage(Image.open(images[1]).resize((132, 100), Image.LANCZOS))
-menuitem_2 = Label(frame, image=menuimage_2, bd=0, cursor="hand2")
-menuitem_2.place(relx=0.5, rely=0.15, anchor=CENTER)
-menuitem_2.bind("<Button-1>", lambda e: mkclone(images[1], 40, 66, 132, 100, 70, 50, "azimuth"))
+menutitle = Label(frame, text="Control panels", font=("", 16))
+menutitle.place(relx=0.5, rely=0.05, anchor=CENTER)
 
-menuimage_3 = ImageTk.PhotoImage(Image.open(images[2]).resize((132, 100), Image.LANCZOS))
-menuitem_3 = Label(frame, image=menuimage_3, bd=0, cursor="hand2")
-menuitem_3.place(relx=0.5, rely=0.45, anchor=CENTER)
-menuitem_3.bind("<Button-1>", lambda e: mkclone(images[2], 40, 280, 132, 100, 70, 50, "tunnel-split-handle"))
 
-menuimage_4 = ImageTk.PhotoImage(Image.open(images[3]).resize((183, 100), Image.LANCZOS))
-menuitem_4 = Label(frame, image=menuimage_4, bd=0, cursor="hand2")
-menuitem_4.place(relx=0.5, rely=0.6, anchor=CENTER)
-menuitem_4.bind("<Button-1>", lambda e: mkclone(images[3], 20, 390, 183, 100, 92.5, 50, "tunnel-dual-screen"))
+def switch():
+    global screens
+    if screens := not screens:
+        menuimage_1 = ImageTk.PhotoImage(Image.open(images[0]).resize((132, 100), Image.LANCZOS))
+        menuitem_1 = Label(frame, image=menuimage_1, bd=0, cursor="hand2")
+        menuitem_1.place(relx=0.5, rely=0.3, anchor=CENTER)
+        menuitem_1.bind("<Button-1>", lambda e: mkclone(images[0], 40, 170, 132, 100, 70, 50, 0))
+        menuitem_1.image = menuimage_1
 
+        menuimage_2 = ImageTk.PhotoImage(Image.open(images[1]).resize((132, 100), Image.LANCZOS))
+        menuitem_2 = Label(frame, image=menuimage_2, bd=0, cursor="hand2")
+        menuitem_2.place(relx=0.5, rely=0.15, anchor=CENTER)
+        menuitem_2.bind("<Button-1>", lambda e: mkclone(images[1], 40, 66, 132, 100, 70, 50, 1))
+        menuitem_2.image = menuimage_2
+
+        menuimage_3 = ImageTk.PhotoImage(Image.open(images[2]).resize((132, 100), Image.LANCZOS))
+        menuitem_3 = Label(frame, image=menuimage_3, bd=0, cursor="hand2")
+        menuitem_3.place(relx=0.5, rely=0.45, anchor=CENTER)
+        menuitem_3.bind("<Button-1>", lambda e: mkclone(images[2], 40, 280, 132, 100, 70, 50, 2))
+        menuitem_3.image = menuimage_3
+
+        menuimage_4 = ImageTk.PhotoImage(Image.open(images[3]).resize((183, 100), Image.LANCZOS))
+        menuitem_4 = Label(frame, image=menuimage_4, bd=0, cursor="hand2")
+        menuitem_4.place(relx=0.5, rely=0.6, anchor=CENTER)
+        menuitem_4.bind("<Button-1>", lambda e: mkclone(images[3], 20, 390, 183, 100, 92.5, 50, 3))
+        menuitem_4.image = menuimage_4
+
+        nextimg = ImageTk.PhotoImage(Image.open("assets/navarrow.png").resize((40, 40), Image.LANCZOS))
+        nextpage = Label(frame, image=nextimg, bd=0, cursor="hand2", bg="grey")
+        nextpage.bind("<Button-1>", lambda e: switch())
+        nextpage.place(relx=0.7, rely=0.7)
+        nextpage.image = nextimg
+    else:
+        menuimage_1 = ImageTk.PhotoImage(Image.open(images[0]).resize((132, 100), Image.LANCZOS))
+        menuitem_1 = Label(frame, image=menuimage_1, bd=0, cursor="hand2")
+        menuitem_1.place(relx=0.5, rely=0.3, anchor=CENTER)
+        menuitem_1.bind("<Button-1>", lambda e: mkclone(images[0], 40, 170, 132, 100, 70, 50, 0))
+        menuitem_1.image = menuimage_1
+
+        menuimage_1 = ImageTk.PhotoImage(Image.open(images[0]).resize((132, 100), Image.LANCZOS))
+        menuitem_1 = Label(frame, image=menuimage_1, bd=0, cursor="hand2")
+        menuitem_1.place(relx=0.5, rely=0.3, anchor=CENTER)
+        menuitem_1.bind("<Button-1>", lambda e: mkclone(images[0], 40, 170, 132, 100, 70, 50, 0))
+        menuitem_1.image = menuimage_1
+
+        menuimage_1 = ImageTk.PhotoImage(Image.open(images[0]).resize((132, 100), Image.LANCZOS))
+        menuitem_1 = Label(frame, image=menuimage_1, bd=0, cursor="hand2")
+        menuitem_1.place(relx=0.5, rely=0.3, anchor=CENTER)
+        menuitem_1.bind("<Button-1>", lambda e: mkclone(images[0], 40, 170, 132, 100, 70, 50, 0))
+        menuitem_1.image = menuimage_1
+
+        menuimage_1 = ImageTk.PhotoImage(Image.open(images[0]).resize((132, 100), Image.LANCZOS))
+        menuitem_1 = Label(frame, image=menuimage_1, bd=0, cursor="hand2")
+        menuitem_1.place(relx=0.5, rely=0.3, anchor=CENTER)
+        menuitem_1.bind("<Button-1>", lambda e: mkclone(images[0], 40, 170, 132, 100, 70, 50, 0))
+        menuitem_1.image = menuimage_1
+
+
+switch()
+validate()
 # Content ends here
 window.mainloop()
--- jsonfile.py
+++ jsonfile.py
@@ -3,7 +3,6 @@ def readvalue(thrustertype):
     file = open("Thrusters.json")
     data = json.load(file)
     for thruster in data['thrusters']:
-        for i in data['thrusters'][thruster]:
-            if thrustertype in data['thrusters'][thruster]:
-                return data['thrusters'][thruster][thrustertype]['properties']['amount']
+        if thrustertype in data['thrusters'][thruster]:
+            return data['thrusters'][thruster][thrustertype]['properties']['amount']
     return None
